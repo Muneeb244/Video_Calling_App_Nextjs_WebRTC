7e842171b5cfe4a71f49d07194ab4189ad6f30b1
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2024-07-21 00:17:14 +0500

    socket setup

---
+++ src/context/socket.js
@@ -0,0 +1,28 @@
+const { useState, useEffect, createContext, useContext } = require("react");
+const { io } = require("socket.io-client");
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+    const socket = useContext(SocketContext);
+    return socket
+}
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const connection = io();
+    console.log(connection)
+    setSocket(connection);
+  }, []);
+
+  socket?.on('connect_error', async (err) => {
+    console.log("Error establishing socket", err)
+    await fetch('/api/socket')
+  })
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
